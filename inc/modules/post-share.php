ef92d5a144808d55994ab0be2f2e6047335ea649
franszo <josh@kishandchips.com>
2015-01-30 16:11:02 +0000

    Fixed share module & cleaned up CSS
    Signed-off-by: franszo <josh@kishandchips.com>

--- inc/modules/post-share.php
+++ inc/modules/post-share.php
@@ -1,8 +1,10 @@
 <div class="post-share clearfix">
 	<div class="share">
 		<span class="title"><?php _e("Join the", THEME_NAME); ?><i><?php _e(" Conversation:", THEME_NAME); ?></i></span>
-		<?php get_template_part('inc/modules/share-links'); ?>
+		<?php if(is_single()): ?>
+			<?php get_template_part('inc/modules/share-links-large'); ?>
+		<?php else: ?>
+			<?php get_template_part('inc/modules/share-links'); ?>
+		<?php endif; ?>
 	</div>
-	
-	<?php // get_template_part('inc/modules/post-meta'); ?>
 </div>
--- inc/modules/share-links.php
+++ inc/modules/share-links.php
@@ -1,14 +1,11 @@
 <ul class="share-links">
 	<li>
-		<a class="leave-comment-btn" href="#" ><?php _e('Leave a comment', 'greenforallseasons'); ?></a>
+		<a class="facebook-btn share-popup-btn" target="_blank" href="http://www.facebook.com/share.php?u=<?php echo urlencode($url); ?>&title=<?php echo urlencode($title); ?>"></a>
 	</li>
 	<li>
-		<a class="facebook-btn share-popup-btn" target="_blank" href="http://www.facebook.com/share.php?u=<?php echo urlencode($url); ?>&title=<?php echo urlencode($title); ?>"><span><?php _e('Share', 'greenforallseasons'); ?></span></a>
+		<a class="twitter-btn share-popup-btn" target="_blank" href="http://twitter.com/home?status=<?php echo urlencode($title); ?>+<?php echo urlencode($url); ?>"></a>
 	</li>
 	<li>
-		<a class="twitter-btn share-popup-btn" target="_blank" href="http://twitter.com/home?status=<?php echo urlencode($title); ?>+<?php echo urlencode($url); ?>"><span><?php _e('Tweet', 'greenforallseasons'); ?></span></a>
-	</li>
-	<li>
-		<a class="pinterest-btn share-popup-btn" target="_blank" href="http://www.pinterest.com/pin/create/button/?url=<?php echo urlencode($url); ?>&media=<?php echo urlencode($image_url); ?>&description=<?php echo urlencode($excerpt); ?>" ><span><?php _e('Pin', 'greenforallseasons'); ?></span></a>
+		<a class="pinterest-btn share-popup-btn" target="_blank" href="http://www.pinterest.com/pin/create/button/?url=<?php echo urlencode($url); ?>&media=<?php echo urlencode($image_url); ?>&description=<?php echo urlencode($excerpt); ?>" ></a>
 	</li>
 </ul>
