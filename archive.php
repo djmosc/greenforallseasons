e2d612a48834589ecee57e0819ab2b0883e337dd
franszo <josh@kishandchips.com>
2015-02-09 14:30:08 +0000

    category & archive page tempaltes
    Signed-off-by: franszo <josh@kishandchips.com>

--- archive.php
+++ archive.php
@@ -1,73 +1,91 @@
-<?php get_header(); ?>
-<section id="archive">
-	<div class="container inner">
-		
-		<div class="posts clearfix">
-			<div class="filters">
-				<div class="inner">
-					<h2 class="title"><?php _e("Archive", THEME_NAME); ?></h2>
-					<p class="label">
-						<?php _e("Viewing:", THEME_NAME); ?>
+<?php
+get_header();
+$category = get_post_category();
+?>
 
-						<span class="red"><?php echo (single_term_title('', false)) ? single_term_title() : 'All'; ?></span>
-					</p>
-					<form mathod="GET">
-						<div class="field">
-							<?php wp_dropdown_categories(array('class' => 'category', 'show_option_all' => __("All Categories", THEME_NAME))); ?>
-						</div>
-						<div class="field">
-							<select class="date">
-								<option value=""><?php _e("Select a date", THEME_NAME); ?></option>
-								<?php wp_get_archives(array('format' => 'option')); ?>
-							</select>
-						</div>
-						<div class="field">
-							<input type="text" class="query" name="s" placeholder="Search" value="<?php echo esc_attr( get_search_query() ); ?>" />
-							<button type="submit" class="submit-btn search-btn" ></button>
-						</div>
-					</form>
-				</div>
-			</div>
-			<?php if(have_posts()): ?>
-			<?php while ( have_posts() ) : the_post(); ?>
-			<?php if( has_post_thumbnail() ): ?>
-			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-				<a class="overlay-btn btn" href="<?php the_permalink(); ?>">
-                    <div class="thumbnail">
-                        <?php the_post_thumbnail('thumbnail'); ?>
-                    </div>
-                    <div class="overlay">
-                        <header class="header">
-                            <h5 class="title"><?php the_title();?></h5>
-                            <div class="meta"><span class="date"><?php the_time(get_option('date_format')); ?></span></div>
-                            <span class="primary-btn arrow-right-btn read-more-btn"><?php _e("Read", THEME_NAME); ?></span>
-                        </header>
-                    </div>
-                </a>
-			</article>
-			<?php endif; ?>
-			<?php endwhile; // end of the loop. ?>
-			<?php else: ?>
+	<main id="main" class="site-main" role="main">
+		<section id="category" class="content-area">
+		<?php if ( have_posts() ) : ?>
+
+			<header class="category-header">
+				<h1 class="category-title"><?php _e('Archive'); ?></h1>
+				<div class="category-name"><?php _e('Viewing '); ?><span><?php echo $category->name; ?></span></div>
+				<div class="filters">
+					<div class="inner">
+						<form mathod="GET">
+							<div class="field">
+								<input type="text" class="query" name="s" placeholder="Search" value="<?php echo esc_attr( get_search_query() ); ?>" />
+								<button type="submit" class="submit-btn search-btn" ></button>
+							</div>						
+							<div class="field">
+								<?php wp_dropdown_categories(array('class' => 'category', 'show_option_all' => __("All Categories", THEME_NAME))); ?>
+							</div>
+							<div class="field">
+								<select class="date">
+									<option value=""><?php _e("Sort by date", THEME_NAME); ?></option>
+									<?php wp_get_archives(array('format' => 'option')); ?>
+								</select>
+							</div>
+						</form>
+					</div>
+				</div>				
+			</header>
+
+			<ul class="posts">
+				<?php 
+				$i = 0;
+				while ( have_posts() ) : the_post(); ?>
+
+					<?php 
+						$title =  get_the_title();
+						if ($i == 0) {
+							$image_size = array('width' => 515, 'height' => 345);
+						} else {
+							$image_size = array('width' => 250, 'height' => 250);
+						}
+	                    $image = get_post_thumbnail_src($image_size);							
+						$excerpt = get_excerpt(150);
+						$url = get_permalink();	
+						$author_img_url = get_avatar_url ( get_the_author_meta('ID'), $size = '40' );
+						$author_id = get_the_author_meta('ID');
+						$date = get_the_time('F d, Y')		
+					?>
+
+
+		            <li>
+		                <?php include_module('post-item', array(
+							'title' => $title,
+							'excerpt' => $excerpt,
+							'url' => $url,
+							'image_url' => $image,
+							'author' => array(
+								'name' => 'Words by ' .get_the_author(),
+								'image_url' => $author_img_url,
+								'url' => get_author_posts_url($author_id),
+							),							
+		                    'category' => array(
+	                        	'name' => $category->name,
+	                        ),
+							'read_more' => $url,
+							'date' => $date,
+						)); ?>
+
+		            </li>								
+				<?php 
+				$i++; 
+				endwhile; // end of the loop. ?>
+		</ul>
+
+		<?php else: ?>
 			<div class="not-found">
 				<h3 class="title"><?php _e("No posts found", THEME_NAME); ?></h3>
 			</div>
-			<?php endif; ?>
-		</div>
-		<footer class="archive-footer footer">
-			<div class="pagination">
-				<?php
-				global $wp_query;
+		<?php endif; ?>
 
-				$big = 999999999;
-				echo paginate_links( array(
-					'base' => str_replace( $big, '%#%', esc_url( get_pagenum_link( $big ) ) ),
-					'format' => '?paged=%#%',
-					'current' => max( 1, get_query_var('paged') ),
-					'total' => $wp_query->max_num_pages
-				) );
-				?>
-			</div>
-		</footer>
-	</div>
-</section><!-- #page -->
+		<?php include_module('pagination'); ?>
+
+		</section>
+	</main>
+	
+
 <?php get_footer(); ?>
